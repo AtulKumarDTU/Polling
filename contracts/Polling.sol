e6944d7b3b7c1017aa82a52b7d0e1f8df8572ee5
AtulKumarDTU <absolute0gravity@gmail.com>
2019-07-12 00:05:52 +0530

    Initial Commit

---
+++ contracts/Polling.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Polling {
+	//store candidate
+	//read candidate
+	string public candidate;
+	//constructor
+	constructor() public {
+		candidate = "Narendra Modi";
+	}
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var Polling = artifacts.require("./Polling.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Polling);
+};
